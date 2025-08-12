2961347007fe94dbf54b9d8c2a1adfb6d439b11a
Charles Chiou <samurai@selfso.com>
2025-08-12 14:54:21 +0800

    Add application versioning
    Signed-off-by: Charles Chiou <samurai@selfso.com>

--- meshmon.cxx
+++ meshmon.cxx
@@ -6,11 +6,18 @@
 
 #include <signal.h>
 #include <getopt.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <mosquitto.h>
+#include <libconfig.h++>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <MeshShell.hxx>
 #include "MeshMon.hxx"
+#include "version.h"
+
+using namespace libconfig;
 
 #define DEFAULT_DEVICE "/dev/ttyAMA0"
 
@@ -48,14 +55,106 @@ void cleanup(void)
     mosquitto_lib_cleanup();
 }
 
+static void loadLibConfig(Config &cfg, string &path)
+{
+    int fd;
+
+    if (path.empty()) {
+        string home;
+
+        home = getenv("HOME");
+        if (home.empty()) {
+            return;
+        }
+
+        path = home + "/.meshmon";
+    }
+
+    // 'touch' to test the path validity
+    fd = open(path.c_str(),
+              O_WRONLY | O_CREAT | O_NOCTTY | O_NONBLOCK,
+              0666);
+    if (fd == -1) {
+        goto done;
+    } else {
+        close(fd);
+        fd = -1;
+    }
+
+    try {
+        cfg.readFile(path.c_str());
+    } catch (FileIOException &e) {
+    } catch (ParseException &e) {
+    }
+
+done:
+
+    return;
+}
+
 int main(int argc, char **argv)
 {
     int ret = 0;
+    Config cfg;
+    string cfgfile;
     vector<string> devices;
     bool useStdioShell = false;
     uint16_t port = 0;
     bool verbose = false;
     bool log = false;
+    string banner;
+    string version;
+    string built;
+    string copyright;
+
+    banner = "The MeshMon Application";
+    version = string("Version: ") + string(MYPROJECT_VERSION_STRING);
+    built = string("Built: ") + string(MYPROJECT_WHOAMI) + string("@") +
+        string(MYPROJECT_HOSTNAME) + string(" ") + string(MYPROJECT_DATE);
+    copyright = string("Copyright (C) 2025, Charles Chiou");
+
+    loadLibConfig(cfg, cfgfile);
+
+    try {
+        Setting &root = cfg.getRoot();
+        Setting &cfgDevices = root["devices"];
+        for (int i = 0; i < cfgDevices.getLength(); i++) {
+            string cfgDevice = cfgDevices[i];
+            if (find(devices.begin(), devices.end(), cfgDevice) ==
+                devices.end()) {
+                devices.push_back(cfgDevice);
+            }
+        }
+    } catch (SettingNotFoundException &e) {
+    } catch (SettingTypeException &e) {
+    }
+
+    try {
+        int cfgStdioShell = 0;
+        Setting &root = cfg.getRoot();
+        root.lookupValue("stdioShell", cfgStdioShell);
+        useStdioShell = cfgStdioShell != 0 ? true : false;
+    } catch (SettingNotFoundException &e) {
+    } catch (SettingTypeException &e) {
+    }
+
+    try {
+        int cfgDeviceLog = 0;
+        Setting &root = cfg.getRoot();
+        root.lookupValue("deviceLog", cfgDeviceLog);
+        log = cfgDeviceLog != 0 ? true : false;
+    } catch (SettingNotFoundException &e) {
+    } catch (SettingTypeException &e) {
+    }
+
+    try {
+        int cfgPort = 0;
+        Setting &root = cfg.getRoot();
+        root.lookupValue("port", cfgPort);
+        port = cfgPort;
+    } catch (SettingNotFoundException &e) {
+    } catch (SettingTypeException &e) {
+    }
 
     for (;;) {
         int option_index = 0;
@@ -118,6 +217,10 @@ int main(int argc, char **argv)
 
             if (useStdioShell && (stdioShell == NULL)) {
                 stdioShell = make_shared<MeshShell>();
+                stdioShell->setBanner(banner);
+                stdioShell->setVersion(version);
+                stdioShell->setBuilt(built);
+                stdioShell->setCopyright(copyright);
                 stdioShell->setClient(mon);
                 stdioShell->setNVM(mon);
             }
@@ -125,6 +228,10 @@ int main(int argc, char **argv)
             if (port != 0) {
                 shell = make_shared<MeshShell>();
                 shell->setClient(mon);
+                shell->setBanner(banner);
+                shell->setVersion(version);
+                shell->setBuilt(built);
+                shell->setCopyright(copyright);
                 shell->bindPort(port);
                 shell->setNVM(mon);
                 netShells.push_back(shell);
